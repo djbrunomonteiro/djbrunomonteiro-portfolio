6240657fd9cfe50fd86556d35a39eef36b58368a
djbrunomonteiro <brunomonteiroestudio@gmail.com>
2022-11-23 22:23:03 -0300

    backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'djbrunomonteiro-site2023';
+  title = 'DJBrunomonteiro';
+
+  constructor(private titleRef: Title ){
+    this.titleRef.setTitle(this.title)
+
+  }
 }
